bcd96da4b0ebaa9e10ba1ab82498c3c0a514da20
mattmiller1996 <mkmiller96@gmail.com>
2025-04-13 19:54:10 -0400

    create RMD file and doc

--- covidgroup.py
+++ covidgroup.py
@@ -5,10 +5,10 @@ df = pd.read_csv("Data/us_counties_covid19_daily.csv")
 
 # Group by county name and sum up total cases
 # If there's a "state" column too, you can group by ["county", "state"] for clarity
-total_cases_by_county = df.groupby(["state", "county"])["cases"].sum().reset_index()
+total_cases_by_county = df.groupby(["state", "county", "fips"])["cases"].sum().reset_index()
 
 # Rename the column for clarity
-total_cases_by_county.rename(columns={"cases": "total_cases"}, inplace=True)
+total_cases_by_county.rename(columns={"cases": "total_cases", "fips": "COUNTYFIPS"}, inplace=True)
 
 # Sort descending by case count
 total_cases_by_county = total_cases_by_county.sort_values(by="total_cases", ascending=False)
